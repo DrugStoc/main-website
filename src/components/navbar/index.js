f30acfffffb7fab17f9d8697ff39cbaaa1998489
techstackmedia <osagienoah04@gmail.com>
2022-09-04 09:44:26 -0700

    comment out SHOP ON WEB button Nav Link in index.js

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -255,13 +255,13 @@ const NavbarLayout = () => {
               </NavLink>
             </Nav>
             <Nav className="ml-auto">
-               <Nav.Link
+               {/* <Nav.Link
                 href="https://app.drugstoc.com"
                 target="_blank"
                 rel="nopener noreferrer"
                 className="nav-button">
                 SHOP ON WEB
-              </Nav.Link>
+              </Nav.Link> */}
 
               <Dropdown>
                 <Dropdown.Toggle className="toggle_blue">
