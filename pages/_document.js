d8a637611beccebeb30e5752ce11f3371e8ce769
techstackmedia1 <osagienoah04@gmail.com>
2023-04-23 11:20:16 -0700

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -36,13 +36,25 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {this.props.styleTags}
-          <meta name="title" content="DrugStoc | Anti Counterfeit Pharmaceuticals, Medical Devices & Consumables" />
-          <meta name="description" content="DrugStoc is Africa's leading e-healthcare pharmaceutical distribution company. Our anti-counterfeit pharmaceuticals, healthcare services, medical supplies and equipment help address the challenges in the pharmaceutical supplychain." />
-          <meta name="keywords" content="top pharmaceutical distribution company, best pharmaceutical distribution company in Nigeria, pharmaceutical company, pharmaceutical supply company lagos,anti-counterfeit supply chain, best pharmaceutical supply chain company, wholesale pharmacy in nigeria, medical devices, consumables, pharmaceutical wholesalers in lagos, pharmaceutical supply companies near lekki, where to buy wholesale drugs in lagos" />
+          <meta
+            name="title"
+            content="DrugStoc | Anti Counterfeit Pharmaceuticals, Medical Devices & Consumables"
+          />
+          <meta
+            name="description"
+            content="DrugStoc is Africa's leading e-healthcare pharmaceutical distribution company. Our anti-counterfeit pharmaceuticals, healthcare services, medical supplies and equipment help address the challenges in the pharmaceutical supplychain."
+          />
+          <meta
+            name="keywords"
+            content="top pharmaceutical distribution company, best pharmaceutical distribution company in Nigeria, pharmaceutical company, pharmaceutical supply company lagos,anti-counterfeit supply chain, best pharmaceutical supply chain company, wholesale pharmacy in nigeria, medical devices, consumables, pharmaceutical wholesalers in lagos, pharmaceutical supply companies near lekki, where to buy wholesale drugs in lagos"
+          />
           <meta name="robots" content="index, follow" />
           <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="language" content="English" />
-          <meta name="google-site-verification" content="YxcXtu0Zz5s25clei5L1g6OQV_bZnLCSpAZhQILtpSk" />
+          <meta
+            name="google-site-verification"
+            content="YxcXtu0Zz5s25clei5L1g6OQV_bZnLCSpAZhQILtpSk"
+          />
           <meta name="mobile-web-app-capable" content="yes" />
 
           <link rel="icon" href="/favicon.ico" />
@@ -87,13 +99,44 @@ export default class MyDocument extends Document {
             }`,
             }}
           />
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: `
+          {
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "url": "https://drugstoc.com/",
+            "logo": "https://res.cloudinary.com/bizstak/image/upload/v1681956861/drugstoc-logo_v08uia.png"
+          }
+        `,
+            }}
+          />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-ZNB75VJZBK"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZNB75VJZBK')
+            `,
+            }}
+          />
         </Head>
         <body>
           <HackyFixFOUC />
           <Main />
           <NextScript />
         </body>
-        <script type="text/javascript" async="" src="//code.tidio.co/ogocxh3zcgb4cdykhwgct7zwamm62w9e.js"></script>
+        <script
+          type="text/javascript"
+          async=""
+          src="//code.tidio.co/ogocxh3zcgb4cdykhwgct7zwamm62w9e.js"
+        ></script>
       </Html>
     );
   }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -116,6 +116,20 @@ const Home = () => {
         `,
           }}
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZNB75VJZBK"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZNB75VJZBK')
+            `,
+          }}
+        />
       </Head>
 
       <Header>
