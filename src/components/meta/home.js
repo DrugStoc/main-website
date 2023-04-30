eedd7d58bb9edf5e277962996ab1f81b752d4598
techstackmedia1 <osagienoah04@gmail.com>
2023-04-29 18:39:04 -0700

    remove 'googletagmanager' and 'GA_TRACKING_ID' script from the home file

--- src/components/meta/home.js
+++ src/components/meta/home.js
@@ -29,22 +29,6 @@ const HomeMetadata = () => {
           site: '@Drugstoc',
         }}
       />
-      <Head>
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.GA_TRACKING_ID}')
-            `,
-          }}
-        />
-      </Head>
     </>
   );
 };
