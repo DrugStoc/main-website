6f99f6b1f4c8e6978344253c842c2710c366e86c
techstackmedia1 <osagienoah04@gmail.com>
2023-05-01 04:21:23 -0700

    careers page metadata

---
+++ src/components/meta/career.js
@@ -0,0 +1,59 @@
+import { NextSeo } from 'next-seo';
+
+const CareerMetadata = () => {
+  return (
+    <>
+      <NextSeo
+        title="Careers | DrugStoc: Join Our Team and Help Unlock Africa's Potential Together - Available Job Openings"
+        description="Unlock Africa's potential and solve healthcare challenges with a team of bright minds. Join us and enjoy comprehensive medical insurance, flexible working hours for new parents, learning and development opportunities, and more. Check out our available vacancies and apply now."
+        keyword={[
+          'Careers',
+          'Healthcare',
+          'African potential',
+          'Opportunities',
+          'Benefits',
+          'Learning and development',
+          'Smart colleagues',
+          'Family-friendly',
+          'Work-life balance',
+          'Purpose',
+          'Well-being',
+          'Scale',
+          'Impact',
+          'Africa',
+        ]}
+        openGraph={{
+          url: 'https://drugstoc.com/',
+          title:
+            "Careers | DrugStoc: Join Our Team and Help Unlock Africa's Potential Together",
+          description:
+            "Unlock Africa's potential and solve healthcare challenges with a team of bright minds. Join us and enjoy comprehensive medical insurance, flexible working hours for new parents, learning and development opportunities, and more. Check out our available vacancies and apply now.",
+          images: [
+            {
+              url: 'https://res.cloudinary.com/bizstak/image/upload/v1682895328/drugstoc-careers-facebook_py2etw.jpg',
+              width: 1200,
+              height: 630,
+              alt: 'DrugStoc social media thumbnail (twitter card) at 1200 x 630 dimension for the careers page showing text "the leading pharmacy partner for healthcare providers in africa".',
+            },
+          ],
+          siteName: 'Drugstoc',
+        }}
+        twitter={{
+          cardType: 'summary_large_image',
+          handle: '@DrugStocHQ',
+          site: '@Drugstoc',
+          title:
+            'Careers | DrugStoc: Tech-Enabled Procurement Partner in Africa - Available Job Openings',
+          image: {
+            url: 'https://res.cloudinary.com/bizstak/image/upload/v1682895340/drugstoc-careers-twitter_qkhbnm.jpg',
+            width: 1200,
+            height: 675,
+            alt: 'DrugStoc social media thumbnail (twitter card) at 1200 x 675 dimension for the careers page showing text "the leading pharmacy partner for healthcare providers in africa".',
+          },
+        }}
+      />
+    </>
+  );
+};
+
+export default CareerMetadata;
--- src/components/meta/home.js
+++ src/components/meta/home.js
@@ -4,7 +4,7 @@ const HomeMetadata = () => {
   return (
     <>
       <NextSeo
-        title="Home | Anti-Counterfeit Supply Chain for Healthcare Providers"
+        title="Home | DrugStoc: Anti-Counterfeit Supply Chain for Healthcare Providers"
         description="Get high-quality healthcare products, medicines, and supplies from Africa's leading e-healthcare pharmaceutical distribution company. Choose DrugStoc for safe, reliable, and innovative healthcare solutions."
         keyword={[
           'Anti-counterfeit supply chain',
@@ -30,7 +30,7 @@ const HomeMetadata = () => {
         openGraph={{
           url: 'https://drugstoc.com/',
           title:
-            'Home | Anti-Counterfeit Supply Chain for Healthcare Providers',
+            'Home | DrugStoc: Anti-Counterfeit Supply Chain for Healthcare Providers',
           description:
             'Drugstoc provides anti-counterfeit supply chain solutions for healthcare providers in emerging economies. Download the Drugstoc app for Android and iOS.',
           images: [
@@ -38,7 +38,7 @@ const HomeMetadata = () => {
               url: 'https://res.cloudinary.com/bizstak/image/upload/v1681940994/drugstoc-home_maw2dg.png',
               width: 1200,
               height: 630,
-              alt: 'DrugStoc social media thumbnail (open graph) at 1200 x 630 dimension',
+              alt: 'DrugStoc social media thumbnail (open graph) at 1200 x 630 dimension for the home page showing female doctor and the text "empowering healthcare providers across africa".',
             },
           ],
           siteName: 'Drugstoc',
@@ -48,12 +48,12 @@ const HomeMetadata = () => {
           handle: '@DrugStocHQ',
           site: '@Drugstoc',
           title:
-            'Home | Anti-Counterfeit Supply Chain for Healthcare Providers',
+            'Home | DrugStoc: Anti-Counterfeit Supply Chain for Healthcare Providers',
           image: {
             url: 'https://res.cloudinary.com/bizstak/image/upload/v1681940981/drugstoc-twitter_okreqx.png',
             width: 1200,
             height: 675,
-            alt: 'DrugStoc social media thumbnail (twitter card) at 1200 x 675 dimension',
+            alt: 'DrugStoc social media thumbnail (twiter) at 1200 x 675 dimension for the home page showing female doctor and the text "empowering healthcare providers across africa".',
           },
         }}
       />
