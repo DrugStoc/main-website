c487772913c9ac93b6b4bd7cd59a27a4b92f7b12
techstackmedia1 <osagienoah04@gmail.com>
2023-05-02 08:20:01 -0700

    add login button

--- pages/_document.js
+++ pages/_document.js
@@ -2,10 +2,16 @@
 /*                            External Dependencies                           */
 /* -------------------------------------------------------------------------- */
 import Document, { Head, Main, NextScript, Html } from 'next/document';
+import { FacebookProvider, LoginButton } from 'react-facebook';
 import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  componentDidMount() {
+    FB.getLoginStatus(function (response) {
+      console.log('User login status:', response);
+    });
+  }
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -106,6 +112,7 @@ export default class MyDocument extends Document {
               `,
             }}
           />
+
           <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -119,6 +126,13 @@ export default class MyDocument extends Document {
         </Head>
         <body>
           <Main />
+          <LoginButton
+            scope="public_profile,email"
+            onCompleted={data => console.log('User login status:', data)}
+            onError={error => console.log(error)}
+          >
+            Login with Facebook
+          </LoginButton>
           <NextScript />
         </body>
       </Html>
