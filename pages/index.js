bba9e8a6c95a525274a17806e0b5e4fc1d143b35
techstackmedia1 <osagienoah04@gmail.com>
2023-05-10 17:02:58 -0700

    subscribe pop-up form from mailchimp api

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Home from 'pages/home';
 import Layout from 'components/layout';
-// import Modal from 'components/modal';
+import Modal from 'components/modal';
 
 export default function HomePage() {
   return (
