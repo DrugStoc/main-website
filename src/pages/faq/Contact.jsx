c1297b51bd1724342ecf2ec987fb89fee980aecc
Bello Osagie <osagienoah04@gmail.com>
2024-02-07 06:38:55 -0800

    feat: open link in new tab

--- src/pages/faq/Contact.jsx
+++ src/pages/faq/Contact.jsx
@@ -1,8 +1,20 @@
+import { useRouter } from 'next/router';
+
 const Contact = () => {
+  const router = useRouter();
+  const handleContactClick = () => {
+    window.open(
+      'https://api.whatsapp.com/send/?phone=2348036879999&text&type=phone_number&app_absent=0',
+      '_blank'
+    );
+  };
+  
   return (
     <div className="contactInfo">
       <p>Still can't find what you're looking for?</p>
-      <button className="buttonContact">Call us on Whatsapp</button>
+      <button className="buttonContact" onClick={handleContactClick}>
+        Call us on Whatsapp
+      </button>
     </div>
   );
 };
