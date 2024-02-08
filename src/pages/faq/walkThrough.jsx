7348d81e1f754c53edf495dab7dbe15a4d35b849
Bello Osagie <osagienoah04@gmail.com>
2024-02-08 03:22:33 -0800

    walthrough youtube link

--- src/pages/faq/walkThrough.jsx
+++ src/pages/faq/walkThrough.jsx
@@ -1,20 +1,26 @@
 const walkThrough = () => {
+  const handleFirstVideoClick = () => {
+    window.open('https://youtu.be/B3zTzsruSvo', '_blank');
+  };
+
+  const handleSecondVideoClick = () => {
+    window.open('https://youtu.be/4H5SjBwS8vg', '_blank');
+  };
+
+  const handleThirdVideoClick = () => {
+    window.open('https://youtu.be/B3zTzsruSvo', '_blank');
+  };
+
   return (
     <div className="faqVideoSection">
       <div className="faqVideos">
-        <div>
-          <a
-            href="https://youtu.be/B3zTzsruSvo"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img
-              src="https://res.cloudinary.com/bizstak/image/upload/v1707303604/iconContainer_b7i1pe.png"
-              alt=""
-            />
-          </a>
+        <div onClick={handleFirstVideoClick}>
+          <img
+            src="https://res.cloudinary.com/bizstak/image/upload/v1707303604/iconContainer_b7i1pe.png"
+            alt=""
+          />
         </div>
-        <div>
+        <div onClick={handleSecondVideoClick}>
           <a
             href="https://youtu.be/4H5SjBwS8vg"
             target="_blank"
