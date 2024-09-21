6bc9214dd6b1dfc43542b38ef9434e32efb52a17
Osagie Bello <info@techstackmedia.com>
2024-09-22 00:31:41 +0100

    feat: implementation of learning layout page

---
+++ pages/learning.js
@@ -0,0 +1,10 @@
+import Learning from 'pages/learning';
+import Layout from 'components/layout';
+
+export default function LearningPage() {
+  return (
+    <Layout title="Learning">
+      <Learning />
+    </Layout>
+  );
+}
