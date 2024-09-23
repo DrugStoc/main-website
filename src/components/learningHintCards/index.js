ebe038a59ac79bcb3c199778161ad43299cb21ff
Osagie Bello <info@techstackmedia.com>
2024-09-23 22:46:59 +0100

    feat: separate data from logic

--- src/components/learningHintCards/index.js
+++ src/components/learningHintCards/index.js
@@ -1,63 +1,51 @@
+const learningCourses = [
+  {
+    icon: 'https://res.cloudinary.com/bizstak/image/upload/v1727045870/library_books_tkdfsb.svg',
+    title: 'Comprehensive Course Library:',
+    description:
+      'Offer a diverse range of courses covering various medical specialties, from primary care to advanced procedures.',
+  },
+  {
+    icon: 'https://res.cloudinary.com/bizstak/image/upload/v1727045929/timeline_e3kr69.svg',
+    title: 'Flexible Learning:',
+    description:
+      'Allow healthcare providers to learn at their own pace with on-demand access to courses and materials.',
+  },
+  {
+    icon: 'https://res.cloudinary.com/bizstak/image/upload/v1727046016/person_tdqvgz.svg',
+    title: 'Expert Instructors:',
+    description:
+      'Highlight experienced healthcare professionals as instructors, ensuring high-quality content.',
+  },
+  {
+    icon: 'https://res.cloudinary.com/bizstak/image/upload/v1727046075/grade_u9oglg.svg',
+    title: 'Continuing Education Credits:',
+    description:
+      "Emphasize the platform's ability to help providers meet their continuing education requirements.",
+  },
+  {
+    icon: 'https://res.cloudinary.com/bizstak/image/upload/v1727046130/question_answer_m0l2yd.svg',
+    title: 'Interactive Learning:',
+    description:
+      'Showcase features like quizzes, simulations, and case studies to enhance engagement.',
+  },
+];
+
 const LearningHintCard = () => {
   return (
     <div className="whyDrugStoc">
       <div className="whyDrugStocSection">
         <h2>Why DrugStoc</h2>
         <section className="learningCourses">
-          <div className="learningCoursesCard">
-            <div className="learningCoursesCardIcon">
-              <img src="https://res.cloudinary.com/bizstak/image/upload/v1727045870/library_books_tkdfsb.svg" />
+          {learningCourses.map((course, index) => (
+            <div className="learningCoursesCard" key={index}>
+              <div className="learningCoursesCardIcon">
+                <img src={course.icon} alt={course.title} />
+              </div>
+              <h4>{course.title}</h4>
+              <p>{course.description}</p>
             </div>
-            <h4>Comprehensive Course Library:</h4>
-            <p>
-              Offer a diverse range of courses covering various medical
-              specialties, from primary care to advanced procedures.
-            </p>
-          </div>
-
-          <div className="learningCoursesCard">
-            <div className="learningCoursesCardIcon">
-              <img src="https://res.cloudinary.com/bizstak/image/upload/v1727045929/timeline_e3kr69.svg" />
-            </div>
-            <h4>Flexible Learning:</h4>
-            <p>
-              Allow healthcare providers to learn at their own pace with
-              on-demand access to courses and materials.
-            </p>
-          </div>
-
-          <div className="learningCoursesCard">
-            <div className="learningCoursesCardIcon">
-              <img src="https://res.cloudinary.com/bizstak/image/upload/v1727046016/person_tdqvgz.svg" />
-            </div>
-            <h4>Expert Instructors:</h4>
-            <p>
-              Highlight experienced healthcare professionals as instructors,
-              ensuring high-quality content.
-            </p>
-          </div>
-
-          <div className="learningCoursesCard">
-            <div className="learningCoursesCardIcon">
-              <img src="https://res.cloudinary.com/bizstak/image/upload/v1727046075/grade_u9oglg.svg" />
-            </div>
-            <h4>Continuing Education Credits:</h4>
-            <p>
-              Emphasize the platform's ability to help providers meet their
-              continuing education requirements.
-            </p>
-          </div>
-
-          <div className="learningCoursesCard">
-            <div className="learningCoursesCardIcon">
-              <img src="https://res.cloudinary.com/bizstak/image/upload/v1727046130/question_answer_m0l2yd.svg" />
-            </div>
-            <h4>Interactive Learning:</h4>
-            <p>
-              Showcase features like quizzes, simulations, and case studies to
-              enhance engagement.
-            </p>
-          </div>
+          ))}
         </section>
       </div>
     </div>
