f52d200da68a3c9e44c61a84dd295a8fc0036848
Osagie Bello <info@techstackmedia.com>
2024-09-23 02:28:16 +0100

    feat: composition of learning most popular videos component to learning page

--- src/pages/learning/index.js
+++ src/pages/learning/index.js
@@ -1,11 +1,13 @@
 import LearningHero from 'components/learningHero';
 import LearningHintCard from 'components/learningHintCards';
+import LearningPopularVideos from 'components/learningPopularVideos';
 
 const Learning = () => {
   return (
     <>
       <LearningHero />
       <LearningHintCard />
+      <LearningPopularVideos />
     </>
   );
 };
