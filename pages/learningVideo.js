7128b11b9d7a6959c368aa395d8b024b73239deb
Osagie Bello <info@techstackmedia.com>
2024-09-23 22:45:39 +0100

    feat: create learning video page

---
+++ pages/learningVideo.js
@@ -0,0 +1,10 @@
+import Layout from 'components/layout';
+import LearningVideo from 'pages/learning/videos/[id]';
+
+export default function LearningVideoPage() {
+  return (
+    <Layout title="LearningVideo">
+      <LearningVideo />
+    </Layout>
+  );
+}
