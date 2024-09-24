1e4dad59edfd6de483e7b189329dc508821c2471
Osagie Bello <info@techstackmedia.com>
2024-09-24 01:22:23 +0100

    feat: implementation of api to get all videos

---
+++ pages/api/videos.js
@@ -0,0 +1,30 @@
+export default function handler(req, res) {
+  const videos = [
+    {
+      id: 1,
+      title: 'Strategies for Business Resilience in an Inflated Economy',
+      videoTitle: 'Harnessing Data for Decision Making',
+      date: 'May 25, 2023 at 10:00 AM',
+    },
+    {
+      id: 2,
+      title: 'Strategies for Business Resilience in an Inflated Economy',
+      videoTitle: 'Harnessing Data for Decision Making',
+      date: 'May 25, 2023 at 10:00 AM',
+    },
+    {
+      id: 3,
+      title: 'Strategies for Business Resilience in an Inflated Economy',
+      videoTitle: 'Harnessing Data for Decision Making',
+      date: 'May 25, 2023 at 10:00 AM',
+    },
+    {
+      id: 4,
+      title: 'Strategies for Business Resilience in an Inflated Economy',
+      videoTitle: 'Harnessing Data for Decision Making',
+      date: 'May 25, 2023 at 10:00 AM',
+    },
+  ];
+
+  res.status(200).json(videos);
+}
