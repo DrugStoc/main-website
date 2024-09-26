16c10ab0aa8ffa7aeee09328155fd63d0e176f83
Osagie Bello <info@techstackmedia.com>
2024-09-26 15:52:59 +0100

    feat: add featured properties to schema

--- pages/api/videos/[id].js
+++ pages/api/videos/[id].js
@@ -5,23 +5,127 @@ export default function handler(req, res) {
       title: 'Strategies for Business Resilience in an Inflated Economy',
       videoTitle: 'Harnessing Data for Decision Making',
       date: 'May 25, 2023 at 10:00 AM',
+      featured: [
+        {
+          imgSrc:
+            'https://res.cloudinary.com/bizstak/image/upload/v1727287875/Speaker1_e0ogqn.png',
+          title: 'Strategies for Business Resilience in an Inflated Economy',
+          videosCount: '5 videos',
+          articlesCount: '3 articles',
+          duration: '8 hours',
+        },
+        {
+          imgSrc:
+            'https://res.cloudinary.com/bizstak/image/upload/v1727295780/speaker2_b3iccw.png',
+          title: 'Leading Trends in Pharmacy Inventory Management',
+          videosCount: '5 videos',
+          articlesCount: '3 articles',
+          duration: '8 hours',
+        },
+        {
+          imgSrc:
+            'https://res.cloudinary.com/bizstak/image/upload/v1727295858/speaker3_umpkxb.png',
+          title: 'Advancing Financial Literacy in Healthcare Business',
+          videosCount: '5 videos',
+          articlesCount: '3 articles',
+          duration: '8 hours',
+        },
+      ],
     },
     {
       id: 2,
       title: 'Strategies for Business Resilience in an Inflated Economy',
       videoTitle: 'Harnessing Data for Decision Making',
+      featured: [
+        {
+          imgSrc:
+            'https://res.cloudinary.com/bizstak/image/upload/v1727287875/Speaker1_e0ogqn.png',
+          title: 'Strategies for Business Resilience in an Inflated Economy',
+          videosCount: '5 videos',
+          articlesCount: '3 articles',
+          duration: '8 hours',
+        },
+        {
+          imgSrc:
+            'https://res.cloudinary.com/bizstak/image/upload/v1727295780/speaker2_b3iccw.png',
+          title: 'Leading Trends in Pharmacy Inventory Management',
+          videosCount: '5 videos',
+          articlesCount: '3 articles',
+          duration: '8 hours',
+        },
+        {
+          imgSrc:
+            'https://res.cloudinary.com/bizstak/image/upload/v1727295858/speaker3_umpkxb.png',
+          title: 'Advancing Financial Literacy in Healthcare Business',
+          videosCount: '5 videos',
+          articlesCount: '3 articles',
+          duration: '8 hours',
+        },
+      ],
       date: 'May 25, 2023 at 10:00 AM',
     },
     {
       id: 3,
       title: 'Strategies for Business Resilience in an Inflated Economy',
       videoTitle: 'Harnessing Data for Decision Making',
+      featured: [
+        {
+          imgSrc:
+            'https://res.cloudinary.com/bizstak/image/upload/v1727287875/Speaker1_e0ogqn.png',
+          title: 'Strategies for Business Resilience in an Inflated Economy',
+          videosCount: '5 videos',
+          articlesCount: '3 articles',
+          duration: '8 hours',
+        },
+        {
+          imgSrc:
+            'https://res.cloudinary.com/bizstak/image/upload/v1727295780/speaker2_b3iccw.png',
+          title: 'Leading Trends in Pharmacy Inventory Management',
+          videosCount: '5 videos',
+          articlesCount: '3 articles',
+          duration: '8 hours',
+        },
+        {
+          imgSrc:
+            'https://res.cloudinary.com/bizstak/image/upload/v1727295858/speaker3_umpkxb.png',
+          title: 'Advancing Financial Literacy in Healthcare Business',
+          videosCount: '5 videos',
+          articlesCount: '3 articles',
+          duration: '8 hours',
+        },
+      ],
       date: 'May 25, 2023 at 10:00 AM',
     },
     {
       id: 4,
       title: 'Strategies for Business Resilience in an Inflated Economy',
       videoTitle: 'Harnessing Data for Decision Making',
+      featured: [
+        {
+          imgSrc:
+            'https://res.cloudinary.com/bizstak/image/upload/v1727287875/Speaker1_e0ogqn.png',
+          title: 'Strategies for Business Resilience in an Inflated Economy',
+          videosCount: '5 videos',
+          articlesCount: '3 articles',
+          duration: '8 hours',
+        },
+        {
+          imgSrc:
+            'https://res.cloudinary.com/bizstak/image/upload/v1727295780/speaker2_b3iccw.png',
+          title: 'Leading Trends in Pharmacy Inventory Management',
+          videosCount: '5 videos',
+          articlesCount: '3 articles',
+          duration: '8 hours',
+        },
+        {
+          imgSrc:
+            'https://res.cloudinary.com/bizstak/image/upload/v1727295858/speaker3_umpkxb.png',
+          title: 'Advancing Financial Literacy in Healthcare Business',
+          videosCount: '5 videos',
+          articlesCount: '3 articles',
+          duration: '8 hours',
+        },
+      ],
       date: 'May 25, 2023 at 10:00 AM',
     },
   ];
