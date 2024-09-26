fb05c7e337cbdc9e8a6c35eb68f100ce65d312d1
Osagie Bello <info@techstackmedia.com>
2024-09-26 01:03:04 +0100

    feat: use of layout component on learning page

---
+++ pages/learning/index.js
@@ -0,0 +1,10 @@
+import Layout from 'components/layout';
+import Learning from 'pages/learning';
+
+export default function LearningPage() {
+  return (
+    <Layout title="Learning">
+      <Learning />
+    </Layout>
+  );
+}
