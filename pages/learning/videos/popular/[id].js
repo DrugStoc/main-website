75b3e9428a36dfa460417407c4cc30cb9cf2d896
Osagie Bello <info@techstackmedia.com>
2024-09-26 21:48:03 +0100

    feat; change of route path and import of exported popular route path

---
+++ pages/learning/videos/popular/[id].js
@@ -0,0 +1,10 @@
+import Layout from 'components/layout';
+import LearningVideo from 'pages/learning/videos/popular/[id]';
+
+export default function LearningVideoPage() {
+  return (
+    <Layout title="Learning Video">
+      <LearningVideo />
+    </Layout>
+  );
+}
