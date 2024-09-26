c76544bdb552e703a17f52e27b09fb1a1b2a5667
Osagie Bello <info@techstackmedia.com>
2024-09-26 23:02:37 +0100

    feat: change of exported default imported path

---
+++ pages/learning/videos/popular/[slug]/[id]/index.js
@@ -0,0 +1,10 @@
+import Layout from 'components/layout';
+import LearningVideo from 'pages/learning/videos/popular/[slug]/[id]';
+
+export default function LearningVideoPage() {
+  return (
+    <Layout title="Learning Video">
+      <LearningVideo />
+    </Layout>
+  );
+}
