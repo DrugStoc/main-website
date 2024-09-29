0d4dd1bdf25a79914cd47ca60e68af6217ebe984
Osagie Bello <info@techstackmedia.com>
2024-09-29 22:11:02 +0100

    feat: implementation of article detail page

---
+++ pages/learning/courses/articles/[slug]/[id]/index.js
@@ -0,0 +1,10 @@
+import Layout from 'components/layout';
+import LearningVideo from 'pages/learning/courses/articles/[slug]/[id]';
+
+export default function LearningVideoPage() {
+  return (
+    <Layout title="Learning Video">
+      <LearningVideo />
+    </Layout>
+  );
+}
