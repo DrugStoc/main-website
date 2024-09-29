f2fe1d5a906a2384799e0f4efa2465e7b6b38c09
Osagie Bello <info@techstackmedia.com>
2024-09-29 14:00:24 +0100

    revert: change folder name, videos to courses -> delete videos folder

---
+++ pages/learning/videos/features/[slug]/[id]/index.js
@@ -0,0 +1,10 @@
+import Layout from 'components/layout';
+import LearningVideo from 'pages/learning/videos/features/[slug]/[id]';
+
+export default function LearningVideoPage() {
+  return (
+    <Layout title="Learning Video">
+      <LearningVideo />
+    </Layout>
+  );
+}
