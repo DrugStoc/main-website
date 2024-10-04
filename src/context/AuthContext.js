a04dc154358ee1268bc804e3b9f261b30c0b8921
Osagie Bello <info@techstackmedia.com>
2024-10-04 05:33:06 +0100

    feat: implementation of auth context

---
+++ src/context/AuthContext.js
@@ -0,0 +1,42 @@
+import { createContext, useState, useContext, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const savedToken = localStorage.getItem('authToken');
+    if (savedToken) {
+      setToken(savedToken);
+    }
+  }, []);
+
+  const login = data => {
+    setToken(data.token);
+    setUser(data.user);
+    localStorage.setItem('authToken', data.token);
+    setIsModalOpen(false);
+  };
+
+  const logout = () => {
+    setToken(null);
+    setUser(null);
+    localStorage.removeItem('authToken');
+    router.push('/learning');
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ token, user, login, logout, setIsModalOpen, isModalOpen }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
