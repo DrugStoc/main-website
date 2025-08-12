07e739d0945e8f0aceb88be5c0e361bec530d39c
deybee97 <deybollar@gmail.com>
2025-08-12 11:19:38 +0100

    add qr download link

---
+++ pages/app-download.js
@@ -0,0 +1,11 @@
+
+import Layout from 'components/layout';
+import AppDownload from 'pages/app-download';
+
+export default function AppDownloadRedirect() {
+  return (
+    <Layout title="App download fredirect">
+      <AppDownload />
+    </Layout>
+  );
+}
